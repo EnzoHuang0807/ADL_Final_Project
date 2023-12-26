b1f5ca3b64340c3ab1d5f05dc49ecbb0d50adece
Enzo <enzo20030807@gmail.com>
2023-12-26 23:27:05 +0800

    remove API key

--- AutoDAN/eval.sh
+++ AutoDAN/eval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python -W ignore eval.py \
-  --API_key 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 \
+  --API_key \
   --low_memory 1 \
   --model vicuna \
   --input_file ./data/AutoDAN.json \
